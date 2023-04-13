f452ae58c1c3dc864387864be7d6eb5cdef60d17
Oleksandr <olexander11441144@gmail.com>
2023-04-13 17:09:08 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import PushNotifications from "node-pushnotifications";
 
 const regIds = [
+  //TODO: upload this from sama-server
   {
     endpoint:
       "https://fcm.googleapis.com/fcm/send/dDGN3r5SXUQ:APA91bFeDHGP9Ewvc2Su18Og7la4PNxiHfHFfNOHmriJMBld4CErC5BioX-fPtuKhVlEkxOi11_0JRskGV3xiIQG94fYlQPMj9gVhCnI1EiB-T7XR3o6fANElQ7E9Ex4pMtH9-2GYfhl",
     expirationTime: null,
     keys: {
       p256dh:
-        "BHMUZrHFDscS3VsRB8tZAXFJLYvZlgaQukwAgeHc54JOZe9X-GdHYhepFlh50QH_zlpAfkXDo29avciaRJqzTzs",
-      auth: "Ou2Z6b2wRZSejPkSgjykGQ",
+        "BPWgDabsdYv4GF7fxadB7s/QjQoWfk3h//nVbrIauxPVG51f4QeJeL0M4B2IHENOTF6wTn92PLC6IUs0JPCDI78=",
+      auth: "126LJgDuBIzPKloLWUY3Og==",
     },
   },
 ];
 
 const dataObject = {
-  title: "New push notification",
+  title: "Notification",
   topic: "topic",
-  body: "Powered by AppFeel",
+  body: "Message from sama-push-deamon",
   message: "payload",
   badge: 4,
 };
@@ -24,10 +25,9 @@ const dataObject = {
 const settings = {
   web: {
     vapidDetails: {
-      subject: "mailto:sender@example.com",
-      publicKey:
-        "BEDl13AheorvsFF1em9iDmcVVtNe96dzOJac0eZven3TqtreoXvsSfZdPG1xnELHnLVaKXQEzaqReisx9ZKbvsM",
-      privateKey: "axplcD-vHtz9fPvxoRtfAaqhQ3WxMbwIYANWyqjiRas",
+      subject: process.env.SUBJECT,
+      publicKey: process.env.PUBLIC_VAPID_KEY,
+      privateKey: process.env.PRIVATE_VAPID_KEY,
     },
     gcmAPIKey: "gcmkey",
     TTL: 2419200,
@@ -39,10 +39,4 @@ const settings = {
 
 const push = new PushNotifications(settings);
 
-push.send(regIds, dataObject, (err, result) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
-});
+push.send(regIds, dataObject, (err, result) => console.log(err ? err : result));
