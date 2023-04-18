a2f9437d73e130582555cd51413b68358c3796d1
Oleksandr <olexander11441144@gmail.com>
2023-04-18 09:51:18 +0300

    add bull listener

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 import PushNotifications from "node-pushnotifications";
+import Queue from "bull";
 
-const regIds = [
-  //TODO: upload this from sama-server
-  {
-    endpoint:
-      "https://fcm.googleapis.com/fcm/send/dDGN3r5SXUQ:APA91bFeDHGP9Ewvc2Su18Og7la4PNxiHfHFfNOHmriJMBld4CErC5BioX-fPtuKhVlEkxOi11_0JRskGV3xiIQG94fYlQPMj9gVhCnI1EiB-T7XR3o6fANElQ7E9Ex4pMtH9-2GYfhl",
-    expirationTime: null,
-    keys: {
-      p256dh:
-        "BPWgDabsdYv4GF7fxadB7s/QjQoWfk3h//nVbrIauxPVG51f4QeJeL0M4B2IHENOTF6wTn92PLC6IUs0JPCDI78=",
-      auth: "126LJgDuBIzPKloLWUY3Og==",
-    },
+const pushNotificationQueue = new Queue("notification", {
+  redis: {
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOSTNAME,
   },
-];
+});
 
-const dataObject = {
-  title: "Notification",
-  topic: "topic",
-  body: "Message from sama-push-deamon",
-  message: "payload",
-  badge: 4,
+//TODO: move to new file
+const pushNotificationProcess = async (job, done) => {
+  const { endpoint, key_p256dh: p256dh, key_auth: auth } = job.data;
+
+  const dataObject = {
+    title: "Notification",
+    topic: "topic",
+    body: "Message from sama-push-deamon",
+    message: "payload",
+    badge: 4,
+  };
+
+  const settings = {
+    web: {
+      vapidDetails: {
+        subject: process.env.SUBJECT,
+        publicKey: process.env.PUBLIC_VAPID_KEY,
+        privateKey: process.env.PRIVATE_VAPID_KEY,
+      },
+      gcmAPIKey: "gcmkey",
+      TTL: 2419200,
+      contentEncoding: "aes128gcm",
+      headers: {},
+    },
+    isAlwaysUseFCM: false,
+  };
+
+  const push = new PushNotifications(settings);
+  push.send(
+    [
+      {
+        endpoint,
+        expirationTime: null,
+        keys: { p256dh, auth },
+      },
+    ],
+    dataObject,
+    (err, result) => console.log(err ? err : result)
+  );
+
+  done();
 };
 
-const settings = {
-  web: {
-    vapidDetails: {
-      subject: process.env.SUBJECT,
-      publicKey: process.env.PUBLIC_VAPID_KEY,
-      privateKey: process.env.PRIVATE_VAPID_KEY,
-    },
-    gcmAPIKey: "gcmkey",
-    TTL: 2419200,
-    contentEncoding: "aes128gcm",
-    headers: {},
-  },
-  isAlwaysUseFCM: false,
-};
-
-const push = new PushNotifications(settings);
-
-push.send(regIds, dataObject, (err, result) => console.log(err ? err : result));
+pushNotificationQueue.process(pushNotificationProcess);
