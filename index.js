0fec60c9757c9c7715c5d7463a9a5dcb617e4d9e
Oleksandr <olexander11441144@gmail.com>
2023-04-19 12:19:32 +0300

    update logic

--- index.js
+++ index.js
@@ -8,44 +8,44 @@ const pushNotificationQueue = new Queue("notification", {
   },
 });
 
+const settings = {
+  web: {
+    vapidDetails: {
+      subject: process.env.SUBJECT,
+      publicKey: process.env.PUBLIC_VAPID_KEY,
+      privateKey: process.env.PRIVATE_VAPID_KEY,
+    },
+    gcmAPIKey: "gcmkey",
+    TTL: 2419200,
+    contentEncoding: "aes128gcm",
+    headers: {},
+  },
+  isAlwaysUseFCM: false,
+};
+
 //TODO: move to new file
 const pushNotificationProcess = async (job, done) => {
-  const { endpoint, key_p256dh: p256dh, key_auth: auth } = job.data;
+  const { devices, message } = job.data;
 
+  const regDevices = devices.map((el) => {
+    return {
+      endpoint: el.web_endpoint,
+      expirationTime: null,
+      keys: { p256dh: el.web_key_p256dh, auth: el.web_key_auth },
+    };
+  });
+
+  //TODO: reed fields for notification from queue, also add list of allowed fileds for message
   const dataObject = {
-    title: "Notification",
+    title: "Message title",
     topic: "topic",
-    body: "Message from sama-push-deamon",
+    body: message.body,
     message: "payload",
-    badge: 4,
-  };
-
-  const settings = {
-    web: {
-      vapidDetails: {
-        subject: process.env.SUBJECT,
-        publicKey: process.env.PUBLIC_VAPID_KEY,
-        privateKey: process.env.PRIVATE_VAPID_KEY,
-      },
-      gcmAPIKey: "gcmkey",
-      TTL: 2419200,
-      contentEncoding: "aes128gcm",
-      headers: {},
-    },
-    isAlwaysUseFCM: false,
   };
 
   const push = new PushNotifications(settings);
-  push.send(
-    [
-      {
-        endpoint,
-        expirationTime: null,
-        keys: { p256dh, auth },
-      },
-    ],
-    dataObject,
-    (err, result) => console.log(err ? err : result)
+  push.send(regDevices, dataObject, (err, result) =>
+    console.log(err ? err : result)
   );
 
   done();
